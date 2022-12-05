b629d154d3454add5a9c6b32832080185700f9bf
TuranFG <turanfg@code.edu.az>
2022-12-05 22:30:42 +0400

    init

---
+++ Tapshirig1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Tapshirig1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inter first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inter second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum of numbers");
+            Console.WriteLine(a + b);
+        }
+    }
+}
